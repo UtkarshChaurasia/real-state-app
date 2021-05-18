3b918716631ab75b5bfb1b3b080f7dad93e13d3b
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-05-18 22:13:09 +0530

    accounts url

--- backend/real_estate/urls.py
+++ backend/real_estate/urls.py
@@ -1,3 +1,6 @@
+from re import template
+from django import urls
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
@@ -6,5 +9,12 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api-auth/',include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+    
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
