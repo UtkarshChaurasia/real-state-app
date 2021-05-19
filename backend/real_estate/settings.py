8a6da2192abfd13496d35f8136588db5d3564672
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-05-19 21:02:51 +0530

    signup logic added

--- backend/real_estate/settings.py
+++ backend/real_estate/settings.py
@@ -150,6 +150,8 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 FILE_UPLOAD_PERMISSIONS=0o640
 
+AUTH_USER_MODEL = 'accounts.UserAccount'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
