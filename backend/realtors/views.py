7f2fa97f48ffec4cd8560e72f533584534eddf20
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-05-20 15:06:55 +0530

    realtors-migration

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from .models import Realtor
+from .serializers import RealtorSerializer
 
-# Create your views here.
+class RealtorListView(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    pagination_class = None
+
+class RealtorView(RetrieveAPIView):
+    serializer_class = RealtorSerializer
+    queryset = Realtor.objects.all()
+
+
+class TopSellerView(ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = Realtor.objects.filter(top_seller=True)
+    serializer_class  = RealtorSerializer
+    pagination_class = None
